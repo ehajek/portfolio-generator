5c6074237bed0b6e6c9a1e74a61c5a968cdc0a62
EddieH <ehajek@outlook.com>
2021-07-19 20:52:28 -0400

    deve update gerenate

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
 const profileDataArgs = process.argv.slice(2);
-const [name, github, extra] = profileDataArgs;
+const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('./index.html', generatePage(name, github, extra), err => {
+fs.writeFile('./index.html', generatePage(name, github), err => {
   if (err) throw new Error(err);
-  console.log (extra);
+  
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
